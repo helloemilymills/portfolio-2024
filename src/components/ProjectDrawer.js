000d0ecba5e2f745f1adfb373eaa1e757828fab1
helloemilymills <helloemilymills@gmail.com>
2024-01-02 23:40:29 +1000

    project drawer started, fonts added

--- src/components/ProjectDrawer.js
+++ src/components/ProjectDrawer.js
@@ -1,7 +1,11 @@
+import './ProjectDrawer.css'
+
 export default function ProjectDrawer() {
     return (
       <>
-        
+        <div className='drawerContainer'>
+          <h2>Wild Betty</h2>
+        </div>
       </>
     );
   }
