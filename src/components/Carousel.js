0d655be9f456580c5b726fde5a6bd318d4b04333
helloemilymills <helloemilymills@gmail.com>
2024-01-02 10:00:33 +1000

    added example images to carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,31 @@
+import image1 from '../images/example1.jpg';
+import image2 from '../images/example2.jpg';
+import image3 from '../images/example3.jpg';
+import image4 from '../images/example4.jpg';
+
 export default function Carousel() {
+
     return (
       <>
-        
+
+        <div class="carousel-container">
+          <img src={image1} alt=''/>
+          <img src={image2} alt=''/>
+          <img src={image3} alt=''/>
+          <img src={image4} alt=''/>
+
+          {/*Next and previous buttons*/}
+          {/* <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+          <a class="next" onclick="plusSlides(1)">&#10095;</a> */}
+
+          {/*dots*/}
+          {/* <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+          </div> */}
+        </div>     
+
       </>
     );
   }
