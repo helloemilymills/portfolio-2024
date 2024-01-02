9c2d4e6f814b23f5876f6336be4e26ed9d9aa090
helloemilymills <helloemilymills@gmail.com>
2024-01-02 21:33:02 +1000

    sticker svg design added

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,7 +5,7 @@ export default function Carousel({imageA, imageB, imageC}) {
   function changeToSlide(n) {
     let slides = document.getElementsByClassName('slide');
     for (let i = 0; i < slides.length; i++) {
-      slides[i] != slides[n-1] ? slides[i].style.display = 'none' : slides[i].style.display = 'block';
+      slides[i] !== slides[n-1] ? slides[i].style.display = 'none' : slides[i].style.display = 'block';
     }; 
   };
 
@@ -13,9 +13,9 @@ export default function Carousel({imageA, imageB, imageC}) {
       <>
 
         <div className="carousel-container">
-          <img className="slide" id="slide 1" src={imageA} alt=''/>
-          <img className="slide" id="slide 2" src={imageB} alt=''/>
-          <img className="slide" id="slide 3" src={imageC} alt=''/>
+          <img className="slide" id="slide 1" src={imageA} alt=""/>
+          <img className="slide" id="slide 2" src={imageB} alt=""/>
+          <img className="slide" id="slide 3" src={imageC} alt=""/>
         </div>     
 
         {/*dots*/}
