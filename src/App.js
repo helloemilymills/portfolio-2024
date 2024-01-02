4867e8b067520af63aa9886b708b85114f84a851
helloemilymills <helloemilymills@gmail.com>
2024-01-02 22:28:51 +1000

    added logo initials

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Carousel from './components/Carousel.js';
 import Sticker from './components/Sticker.js';
+import Logo from './components/Logo.js';
 
 import image1A from './images/example1.jpg';
 import image1B from './images/example2.jpg';
@@ -9,6 +10,7 @@ import image1C from './images/example3.jpg';
 export default function App() {
   return (
     <>
+      <Logo/>
       <Sticker/>
       <Carousel imageA={image1A} imageB={image1B} imageC={image1C}/>
     </>
