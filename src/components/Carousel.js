860a31c82d0c1eb667bb349c747f35091801d4a4
helloemilymills <helloemilymills@gmail.com>
2024-01-02 19:17:09 +1000

    carousel dot functionality working

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,23 +2,30 @@ import './Carousel.css';
 
 export default function Carousel({imageA, imageB, imageC}) {
 
+  function changeToSlide(n) {
+    let slides = document.getElementsByClassName('slide');
+    for (let i = 0; i < slides.length; i++) {
+      slides[i] != slides[n-1] ? slides[i].style.display = 'none' : slides[i].style.display = 'block';
+    }; 
+  };
+
     return (
       <>
 
-        <div class="carousel-container">
-          <img src={imageA} alt=''/>
-          <img src={imageB} alt=''/>
-          <img src={imageC} alt=''/>
+        <div className="carousel-container">
+          <img className="slide" id="slide 1" src={imageA} alt=''/>
+          <img className="slide" id="slide 2" src={imageB} alt=''/>
+          <img className="slide" id="slide 3" src={imageC} alt=''/>
         </div>     
 
         {/*dots*/}
-        <div class="dots-container">
-          <div class="dot" onclick="currentSlide(1)"></div>
-          <div class="dot" onclick="currentSlide(2)"></div>
-          <div class="dot" onclick="currentSlide(3)"></div>
+        <div className="dots-container">
+          <div className="dot" onClick={() => changeToSlide(1)}></div>
+          <div className="dot" onClick={() => changeToSlide(2)}></div>
+          <div className="dot" onClick={() => changeToSlide(3)}></div>
         </div>
 
       </>
     );
-  }
+  };
   
