cf76c07c3e509273f42e68cbaa1aaa5b1ecf4301
helloemilymills <helloemilymills@gmail.com>
2024-01-02 10:52:35 +1000

    carousel images cover screen, no overflow, initial set up for dots to cycle through carousel images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,3 +1,4 @@
+import './Carousel.css';
 import image1 from '../images/example1.jpg';
 import image2 from '../images/example2.jpg';
 import image3 from '../images/example3.jpg';
@@ -13,18 +14,16 @@ export default function Carousel() {
           <img src={image2} alt=''/>
           <img src={image3} alt=''/>
           <img src={image4} alt=''/>
-
-          {/*Next and previous buttons*/}
-          {/* <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-          <a class="next" onclick="plusSlides(1)">&#10095;</a> */}
-
-          {/*dots*/}
-          {/* <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
-          </div> */}
         </div>     
+
+        {/*dots*/}
+        <div class="dots-container">
+          <div class="dot" onclick="currentSlide(1)"></div>
+          <div class="dot" onclick="currentSlide(2)"></div>
+          <div class="dot" onclick="currentSlide(3)"></div>
+          <div class="dot" onclick="currentSlide(4)"></div>
+          <div class="dot" onclick="currentSlide(5)"></div>
+        </div>
 
       </>
     );
