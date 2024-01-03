55ba44b9dc9fcfe6040c7540d1b2a495f87574e4
helloemilymills <helloemilymills@gmail.com>
2024-01-04 09:52:50 +1000

    added curly brackets to parsed props

--- src/components/ProjectDrawer.js
+++ src/components/ProjectDrawer.js
@@ -1,7 +1,7 @@
 import './ProjectDrawer.css'
 import ButtonCTA from './ButtonCTA';
 
-export default function ProjectDrawer(closeDrawer) {
+export default function ProjectDrawer({closeDrawer}) {
   
      return (
       <>
@@ -11,7 +11,7 @@ export default function ProjectDrawer(closeDrawer) {
             <p>project info here</p>
             <ButtonCTA/>
           </div>
-          <svg onClick = {() => closeDrawer.closeDrawer()}id="closeIcon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg onClick = {() => closeDrawer()}id="closeIcon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z" fill="white"/>
           </svg>
         </div>
