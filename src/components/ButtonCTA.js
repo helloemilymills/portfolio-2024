afd066bcdcbb3fb6027c534afe2b2cdc64358f23
helloemilymills <helloemilymills@gmail.com>
2024-01-03 12:24:33 +1000

    added CTA button (no link yet) to project drawer

--- src/components/ButtonCTA.js
+++ src/components/ButtonCTA.js
@@ -1,6 +1,18 @@
+import './ButtonCTA.css'
+
 export default function ButtonCTA() {
+  
+
+  // function navigateTo() {
+    
+  // }
+
     return (
       <>
+        
+        <div className='button'>
+          <h4>Work With Me</h4>
+        </div>
         
       </>
     );
