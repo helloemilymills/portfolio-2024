90a66ca78783872c6384ee29fe14a5c032a4fe09
helloemilymills <helloemilymills@gmail.com>
2024-02-01 22:40:41 +1000

    fixed some duplicate class names causing issues with the closing cross icons and gradient backgrounds

--- src/components/ProjectDrawer.js
+++ src/components/ProjectDrawer.js
@@ -5,8 +5,8 @@ export default function ProjectDrawer({closeDrawer, closeDrawerOpenContactForm, 
 
      return (
       <>
-        <div className='gradientEdge'>
-          <div className='drawerContainer'>
+        <div className='projectDrawerGradientEdge'>
+          <div className='drawerPanel'>
             <h2>{projectTitle}</h2>
             <p>{projectIntro}</p>
             <p><strong>Notable Elements:</strong></p>
@@ -19,7 +19,7 @@ export default function ProjectDrawer({closeDrawer, closeDrawerOpenContactForm, 
             </ul> */}
             <ButtonCTA text="Work with me" closeDrawerOpenContactForm={closeDrawerOpenContactForm} drawerState={drawerState}/>
           </div>
-          <svg onClick = {() => closeDrawer()} id="closeIcon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg onClick = {() => closeDrawer()} id="drawerCloseIcon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z" fill="white"/>
           </svg>
         </div>
